d9aaaf1566e317131a6a19a654f421bc8e220051
Horsfield, David <dakh@union30.ccs.itd.umich.edu>
2017-04-08 14:13:50 -0400

    Initial Commit

---
+++ hw13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw13
+//
+//  Created by Horsfield, David on 4/8/17.
+//  Copyright © 2017 Horsfield, David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
